f799bc0efb0baccab8877a0da36f042a18dd1f6e
Lorna Sherwood <lornasherwood@gmail.com>
2017-01-09 20:53:50 +0000

    almost complete extension part of homework

---
+++ VetClinicTest.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class VetClinicTest{
+  Zoo zoo;
+  Animal animal;
+  VetClinic vetClinic;
+
+  @Before 
+  public void before() {
+    zoo = new Zoo("Parrot Corner");
+    animal = new Animal();
+    vetClinic = new VetClinic("Dr Doolittle");
+  }
+
+  @Test
+  public void hasName() {
+  assertEquals("Dr Doolittle", vetClinic.getName());
+  }
+
+  @Test
+  public void startsWithNoAnimals(){
+    assertEquals(0, vetClinic.animalCount());
+  }
+
+  @Test
+  public void startsWithNoEnclosures(){
+    assertEquals(0, vetClinic.enclosureCount());
+  }
+
+  @Test
+  public void canAddAnimalAndEnclosure(){
+    vetClinic.treatAnimal(animal, zoo);
+    assertEquals(1, vetClinic.animalCount());
+    assertEquals(1, vetClinic.enclosureCount());
+  }
+
+
+}
+/*
+@Test
+public void canVetVisit() {
+  zoo.addAnimals(animal);
+  zoo.addAnimals(animal);
+  zoo.haveVetVisit(vet);
+  assertEquals(1, zoo.animalCount());
+}
+
+public void haveVetVisit(VetVisit vet) {
+  int lastIndex = animalCount() - 1;
+  enclosure[lastIndex] = null;
+}
+
+
+*/
--- ZooTest.java
+++ ZooTest.java
@@ -48,6 +48,8 @@ public class ZooTest{
     zoo.emptyEnclosure();
     assertEquals(0, zoo.animalCount());
   }
+
+ 
 }
 
 
